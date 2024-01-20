0cfff9393454903ad70e14f2d6d0b9ed43d0cbab
Aromal Balakrishnan <naptelaromal@gmail.com>
2024-01-20 17:09:55 +0530

    db connectivity

--- api/index.js
+++ api/index.js
@@ -1,6 +1,19 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose.connect("mongodb+srv://aromal:aromal@mern.dqpiija.mongodb.net/mern-auth?retryWrites=true&w=majority")
+  .then(() => {
+    console.log("Connected to MongoDb!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
-app.listen(3000, () => { console.log('Server is Listening to port 3000');
-})
+app.listen(3000, () => {
+  console.log("Server is Listening to port 3000");
+});
