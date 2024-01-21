4d3a4d6f2da5891d390fb76f33f6a019e5e96c11
Aromal Balakrishnan <naptelaromal@gmail.com>
2024-01-21 06:41:22 +0530

    api route setup

--- api/index.js
+++ api/index.js
@@ -1,10 +1,22 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRoutes from "./routes/user.route.js"
 
 dotenv.config();
 
-mongoose.connect("mongodb+srv://aromal:aromal@mern.dqpiija.mongodb.net/mern-auth?retryWrites=true&w=majority")
+//const mongoose = require("mongoose");
+/*
+const connectDB = async () => {
+  try {
+    await mongoose.connect("mongodb+srv://aromal:aromal@mern.dqpiija.mongodb.net/mersn-auth?retryWrites=true&w=majority");
+  }
+  catch (error) {
+    console.log(error.message , error)
+  }
+}*/
+
+mongoose.connect(process.env.MONGO)
   .then(() => {
     console.log("Connected to MongoDb!");
   })
@@ -17,3 +29,5 @@ const app = express();
 app.listen(3000, () => {
   console.log("Server is Listening to port 3000");
 });
+
+app.use('/api/user', userRoutes);
